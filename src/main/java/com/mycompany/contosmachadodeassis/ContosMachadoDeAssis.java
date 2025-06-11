76f9d73a49566b5315f40afe71021079dfe3ad22
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2025-06-11 18:30:33 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/contosmachadodeassis/ContosMachadoDeAssis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contosmachadodeassis;
+
+/**
+ *
+ * @author Nicholas Zortea (zorteanicholas619@gmail.com)
+ */
+public class ContosMachadoDeAssis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
