1e13645e1321694d164232dd0ce2589e446d43f0
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2025-06-11 22:31:03 -0300

    Feat: Include new files and dependencies

---
+++ src/main/java/br/com/fbd/contosmachadodeassis/utils/JDBCUtil.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fbd.contosmachadodeassis.utils;
+
+/**
+ *
+ * @author Nicholas Zortea (zorteanicholas619@gmail.com)
+ */
+public class JDBCUtil {
+//    private static String getUser(){
+//
+//    };
+//    private static String getPassword(){
+//
+//    };
+//    private static String getURL(){
+//        
+//    }
+//    
+//    public static Connection getConnection() throws SQLException{
+//        
+//    };
+
+}
